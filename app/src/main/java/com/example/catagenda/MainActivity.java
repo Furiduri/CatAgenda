4f94d634bec414121ad4de50c04c84b217683313
Jorge Osvaldo Perez <jorge.furiduri@gmail.com>
2019-04-03 19:47:58 -0600

    Busca y agrega Usuario

--- app/src/main/java/com/example/catagenda/MainActivity.java
+++ app/src/main/java/com/example/catagenda/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,19 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        final boolean[] flag = {false};
         final Shedul sh = new Shedul();
         final User U = new User();
         final EditText txtNombre = findViewById(R.id.txtNombre);
         final EditText txtBuscar = findViewById(R.id.txtBuscar);
         final Button btnAdd =  findViewById(R.id.btnAdd);
         final Button btnBuscar = findViewById(R.id.btnBuscar);
-
+        //On click Add buttom
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 U.setNombre(txtNombre.getText().toString());
                 sh.AddUser(U);
+            }
+
+        });
+
+        //On clik Buscar
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag[0] = sh.BuscarNombre(txtBuscar.getText().toString());
+                Toast.makeText(getApplication(),"Usuario"+ flag[0],Toast.LENGTH_LONG).show();
             }
         });
     }
--- app/src/main/java/com/example/catagenda/Shedul.java
+++ app/src/main/java/com/example/catagenda/Shedul.java
@@ -1,4 +1,22 @@
 package com.example.catagenda;
 
+import java.util.ArrayList;
+
 class Shedul {
+    ArrayList<User> ListUser;
+
+    public void AddUser(User user){
+        ListUser = new ArrayList<User>();
+        ListUser.add(user);
+    }
+    public boolean BuscarNombre(String Param){
+        boolean flag = false;
+        for (User U: ListUser
+             ) {
+            if(U.getNombre().equals(Param))
+                flag = true;
+        }
+        return flag;
+    }
+
 }
